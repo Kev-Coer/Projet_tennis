389b9c5bb86ebaa3022e47dd08900deea46e6061
Kévin Laurent <K3.Viin@live.be>
2020-08-26 12:28:27 +0200

    ajout DAO et JVConnection pour connexion DB

---
+++ src/be/laurent/dao/DAO.java
@@ -0,0 +1,24 @@
+package be.laurent.dao;
+
+import java.sql.Connection;
+
+
+public abstract class DAO<T> {
+	protected Connection connect = null;
+	
+	public DAO(Connection conn){
+		this.connect = conn;
+	}
+	
+	public abstract boolean create(T obj);
+	
+	public abstract boolean delete(T obj);
+	
+	public abstract boolean update(T obj);
+	
+	public abstract T find(int obj);
+	
+	public abstract T findstring(String obj);
+	public abstract T findEquipe(int obj,int obj1);
+	
+}
